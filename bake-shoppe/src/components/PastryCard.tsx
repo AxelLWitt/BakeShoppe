2bd855683a82c3f77536b777b58af9ac29859b37
rehanhussa <Technosis141@gmail.com>
2023-10-12 21:23:31 -0400

    navbar

--- bake-shoppe/src/components/PastryCard.tsx
+++ bake-shoppe/src/components/PastryCard.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+
 
 interface PastryCardProps {
     photo: string;
@@ -6,23 +8,31 @@ interface PastryCardProps {
     description: string;
     price: string;
     removeUnderLine?: boolean;
-  }
-
-const PastryCard: React.FC<PastryCardProps> = ({photo, name, description, price, removeUnderLine=false}) => {
-    const noBorder = removeUnderLine ? 'no-border': ''
-  return (
-    <div className={`${noBorder} pastryCard`}>
-        <div className='pastryContainer'>
-        <img src={photo} className='pastryPeekIcon'></img>
-            <div>
-                <h1>{name}</h1>
-                <p>{description}</p>
-                <h2>${price}</h2>
-                <button className='buttonClass pastryButton'>Add To Card</button>
-            </div>
-        </div>
-    </div>
-  )
 }
 
-export default PastryCard
+
+const PastryCard: React.FC<PastryCardProps> = ({photo, name, description, price, removeUnderLine=false}) => {
+    const noBorder = removeUnderLine ? 'no-border': '';
+    const [itemCount, setItemCount] = useState(0); // Cart state
+    const [svgCounter, setSvgCounter] = useState(0); // SVG counter state
+    function handleAddToCart (){
+      setItemCount((prevCount) => prevCount + 1);
+      console.log('Added to cart', itemCount);
+      setSvgCounter((prevCounter) => (prevCounter + 1) % 4); // Cycle through 0 to 3
+    };
+    return (
+        <div className={`${noBorder} pastryCard`}>
+            <div className='pastryContainer'>
+                <img src={photo} className='pastryPeekIcon'></img>
+                <div>
+                    <h1>{name}</h1>
+                    <p>{description}</p>
+                    <h2>${price}</h2>
+                    <button className='buttonClass pastryButton' onClick={handleAddToCart}>Add To Card</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PastryCard;
